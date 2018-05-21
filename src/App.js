cf4be82065fb98baeb0a8e04821317c491cbc607
Missy Bur <missybur@gmail.com>
2018-05-21 15:37:22 -0700

    Add feature to remove guests from list

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import GuestList from './GuestList';
 class App extends Component {
 
   state = {
+    isFiltered: false,
+    pendingGuest: "",
     guests: [
       {
         name: 'Treasure',
@@ -43,6 +45,14 @@ class App extends Component {
   toggleEditingAt = index =>
     this.toggleGuestPropertyAt("isEditing", index);
 
+  removeGuestAt = index =>
+    this.setState({
+      guests: [
+        ...this.state.guests.slice(0, index),
+        ...this.state.guests.slice(index +1)
+      ]
+    })
+
   setNameAt = (name, indexToChange) =>
     this.setState({
       guests: this.state.guests.map((guest, index) => {
@@ -56,6 +66,27 @@ class App extends Component {
       })
     });
 
+  toggleFilter = () =>
+    this.setState({ isFiltered: !this.state.isFiltered });
+
+  handleNameInput = e =>
+    this.setState({ pendingGuest: e.target.value });
+
+  newGuestSubmitHandler = e => {
+    e.preventDefault();
+    this.setState({
+      guests: [
+      {
+        name: this.state.pendingGuest,
+        isConfirmed: false,
+        isEditing: false
+      },
+      ...this.state.guests
+      ],
+      pendingGuest: ''
+    });
+  }
+
   getTotalInvited = () => this.state.guests.length;
   // getAttendingGuests = () =>
   // getUnconfirmedGuests = () =>
@@ -65,9 +96,13 @@ class App extends Component {
       <div className="App">
         <header>
           <h1>RSVP</h1>
-          <p>A Treehouse App</p>
-          <form>
-            <input type="text" value="Safia" placeholder="Invite Someone" />
+          <p>A React App</p>
+          <form onSubmit={this.newGuestSubmitHandler}>
+            <input
+            type="text"
+            onChange={this.handleNameInput}
+            value={this.state.pendingGuest}
+            placeholder="Invite Someone" />
             <button type="submit" name="submit" value="submit">Submit</button>
           </form>
         </header>
@@ -75,7 +110,10 @@ class App extends Component {
           <div>
             <h2>Invitees</h2>
             <label>
-              <input type="checkbox" /> Hide those who haven't responded
+              <input
+                type="checkbox"
+                onChange={this.toggleFilter}
+                checked={this.state.isFiltered} /> Hide those who haven't responded
             </label>
           </div>
           <table className="counter">
@@ -100,6 +138,8 @@ class App extends Component {
             toggleConfirmationAt={this.toggleConfirmationAt}
             toggleEditingAt={this.toggleEditingAt}
             setNameAt={this.setNameAt}
+            isFiltered={this.state.isFiltered}
+            removeGuestAt={this.removeGuestAt}
           />
 
         </div>
